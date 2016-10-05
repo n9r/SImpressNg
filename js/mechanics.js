009521211e4cb23570d3fe3edd20512892f810e1
n9r <no9org@gmail.com>
2016-10-05 14:24:17 +0200

    'Mock query' button added for testing purposes.

--- js/mechanics.js
+++ js/mechanics.js
@@ -1,33 +1,30 @@
 var sImpressApp = angular.module("sImpressApp", []);
 
 sImpressApp.controller("currDispCtrl", function ($scope, $http, $filter, $sce){
-	$scope.simJson;
+	$scope.simJson ='';
    $scope.trustedUrl = [];
    $scope.dbQueryLimitFrom = 0;
    $scope.dbQueryLimitTo = 5;
-	$scope.getData = function () {
+	$scope.getData = function (isMock) {
 
+   if (isMock) {
 	   //Real connection
-/*
 	   $scope.promise = $http.get("php/dbquery.php?lf="+$scope.dbQueryLimitFrom+"&lt="+$scope.dbQueryLimitTo);
-*/
-      // Mocked connection
+   } else {
+      // Mocked JSON data 
       $scope.promise = $http.get("php/mockedDatabaseReply.php");
-
+   }
 		$scope.promise.then(function(response) {
 			$scope.simJson = response.data.records;
-		})
+		});
       $scope.promise.then (
          function () {
-         $scope.simJson.forEach(function (item, index, Arr) {
-            var obj = {"trustedpath": "http://soundimpress.pl/"+item.filepath};
+         $scope.simJson.forEach(function (item, index) {
             $scope.simJson[index].trustedpath = $sce.trustAsResourceUrl("http://soundimpress.pl/"+item.filepath);
          });
       });
    };
-
-   $scope.sendQuery = function () {
-      $scope.getData();
+   $scope.sendQuery = function (a) {
+      $scope.getData(a);
    };
-   
 });
